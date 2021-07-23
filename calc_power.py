f53da488635ad791b387775f2d3f672aa5354c97
Subham Sen <subhamsen18@gmail.com>
2021-07-23 22:08:36 +0530

    Code cleaned and functionalized.

--- calc_power.py
+++ calc_power.py
@@ -1,4 +1,11 @@
-num = int(input("Enter a number to calculate power: "))
-power = int(input("Enter the power for the number"))
-print(f"{num} raised to the power {power} is {num ** power}")
+def compute_power(base, index):
+    return base ** index
 
+
+def main():
+    base = int(input("Enter a number to calculate power: "))
+    index = int(input("Enter the power for the number: "))
+    print(f"{base} raised to the power {index} is {compute_power(base, index)}")
+
+if __name__ == "__main__":
+    main()
--- hungry.py
+++ hungry.py
@@ -1,10 +1,14 @@
-print("Hello World")
-hungry = input("I am hungry")
-if hungry == "yes":
-    print("eat up.")
-elif hungry == 'no':
-    print("stop bothering!")
-else:
-    print("Way down we go!")
-print("Edit from another user")
-print("Bye.")
+def main():
+
+    print("Hello World")
+    hungry = input("Are you hungry? ")
+    if hungry.lower() == "yes" or hungry.lower() == "y":
+        print("eat up.")
+    elif hungry.lower() == 'no' or hungry.lower() == "n":
+        print("stop bothering!")
+    else:
+        print("Way down we go!")
+    print("Bye.")
+
+if __name__ == "__main__":
+    main()
